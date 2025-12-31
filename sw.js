852f220b6afaf5eb59c3b8428040f736aed0ba4a
Priyanshu Bej <118946442+PriyanshuJVM@users.noreply.github.com>
2025-12-31 13:36:46 +0530

    Refactor service worker event listeners and notifications

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-// sw.js - The Background Process
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
+self.addEventListener('install', () => self.skipWaiting());
 
-self.addEventListener('activate', (event) => {
-    console.log("2026 Broadcast Service Active");
-});
-
-// This listens for a message from your main site to schedule the alert
 self.addEventListener('message', (event) => {
-    if (event.data.type === 'SCHEDULE_NY_NOTIFICATION') {
-        const delay = event.data.targetTime - Date.now();
-        
+    if (event.data.type === 'SCHEDULE_NOTIFICATION') {
+        const delay = event.data.time - Date.now();
         if (delay > 0) {
             setTimeout(() => {
-                self.registration.showNotification("2026 HAS ARRIVED", {
-                    body: `Happy New Year, ${event.data.name}! Tap to enter the future.`,
+                self.registration.showNotification("2026 BROADCAST", {
+                    body: `Happy New Year, ${event.data.name}! The future has arrived.`,
                     icon: 'https://cdn-icons-png.flaticon.com/512/3361/3361905.png',
-                    vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40],
-                    tag: 'ny-broadcast'
+                    vibrate: [500, 100, 500],
+                    tag: 'ny-2026'
                 });
             }, delay);
         }
