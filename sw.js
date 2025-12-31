9889f07c8f901c375611d768d53f72f2fffd4079
Priyanshu Bej <118946442+PriyanshuJVM@users.noreply.github.com>
2025-12-31 13:41:31 +0530

    Update notification scheduling and message content

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
 
 self.addEventListener('message', (event) => {
-    if (event.data.type === 'SCHEDULE_NOTIFICATION') {
-        const delay = event.data.time - Date.now();
+    if (event.data.type === 'SCHEDULE_NY_ALERT') {
+        const delay = event.data.targetTime - Date.now();
         if (delay > 0) {
             setTimeout(() => {
-                self.registration.showNotification("2026 BROADCAST", {
-                    body: `Happy New Year, ${event.data.name}! The future has arrived.`,
+                self.registration.showNotification("WELCOME TO 2026", {
+                    body: `Happy New Year, ${event.data.name}! Open to see your greeting.`,
                     icon: 'https://cdn-icons-png.flaticon.com/512/3361/3361905.png',
                     vibrate: [500, 100, 500],
-                    tag: 'ny-2026'
+                    tag: 'ny-2026-alert'
                 });
             }, delay);
         }
